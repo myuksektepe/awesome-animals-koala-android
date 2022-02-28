8f6a7f2dcc6c7f27001f276cb0bdc13bab56a171
myuksektepe <yuksektepemurat@gmail.com>
2022-03-01 00:52:45 +0300

    page design

--- app/src/main/java/awesome/animals/koala/util/Constans.kt
+++ app/src/main/java/awesome/animals/koala/util/Constans.kt
@@ -1,5 +1,8 @@
 package awesome.animals.koala.util
 
 const val TAG = "applog"
+
+
 const val API_URL = "https://api.rit.im/obi-dahi/awesome-animals"
 const val BOOK_NAME = "koala"
+const val THEME_COLOR = "#6F45BA" // Change in colors.xml
